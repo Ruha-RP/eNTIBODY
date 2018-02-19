dd474c1a6c04b021c918dca390d2ee0ca6470be4
Ruha Peera <ruha.rizwan@gmail.com>
2018-02-19 00:02:57 -0500

    Functionality: two guards, db tables created, two routes, admin dashbord not accesible through user profile

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card card-default">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">ADMIN Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in as <strong>ADMIN</strong>
                 </div>
             </div>
         </div>
