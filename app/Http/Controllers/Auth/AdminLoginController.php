085f666511630e4f484a0b916035db95f40744e4
Ruha Peera <ruha.rizwan@gmail.com>
2018-02-19 14:15:56 -0500

    adds middelsware in AdminLoginController

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -7,10 +7,15 @@ use App\Http\Controllers\Controller;
 
 class AdminLoginController extends Controller
 {
+	public function __construct()
+	{
+		$this->middleware('guest:admin');
+	}
+
     public function showLoginForm()
 
     {
-    	return view('auth.admin-login')
+    	return view('auth.admin-login');
     }
 
     public function login()
