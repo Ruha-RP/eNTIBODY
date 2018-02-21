a4fb67eee3b990a5296ac259e29acdc279e2ee5c
Ruha Peera <ruha.rizwan@gmail.com>
2018-02-21 11:03:49 -0500

    adds 3 buttons with defined routes in employee screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,20 @@
 
                     @component('components.who')
                     @endcomponent
-                    
                 </div>
+
+                <div class="card-body">
+                    <div class="links">
+                       <!--  Shows Test Info  -->
+                        <button class="button" onclick="location.href='/users/info';">Test Info</button>
+                        <!-- Shows all patients -->
+                        <button class="button" onclick="location.href='/users/patients';">View All Patients</button>
+                        <!-- Sends to Test Screen -->
+                        <button class="button" onclick="location.href='/users/test';">Test Now</button>
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
