7ec0a61acf7a87150f4c1d3f846a6bbb6735d399
Ruha Peera <ruha.rizwan@gmail.com>
2018-02-21 11:32:28 -0500

    adds test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,18 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card card-default">
-                <div class="card-header">ADMIN Dashboard</div>
+                <div class="card-header">USER Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @component('components.who')
-                    @endcomponent
-                    
+                    <h1> TEST SCREEN </h1>
+                    <p>Perform Test here</p>
+                    <p> Active Button: adds patient details</p>
+                    <p> Inactive Button: Start Now, leads to another SOP after activation, happens after patient details input</p>                 
                 </div>
             </div>
         </div>
