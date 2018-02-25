8ce266414823821994f6aaadd582b9b9465cfeb5
Ruha Peera <ruha.rizwan@gmail.com>
2018-02-24 23:20:01 -0500

    placeholdered search bar added to patient.blade

--- resources/views/patients.blade.php
+++ resources/views/patients.blade.php
@@ -10,21 +10,32 @@
                 <div class="card-body">
                     <h1> ALL PATIENTS </h1>
                     <p>Click on a patient for more Details</p>
-                    <p>Search by PID or Name</p>
+
+                    <form class="navbar-form navbar-right" role="search">
+                      <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search by PID or Name">
+                      </div>
+                      <button type="submit" class="button">Submit</button>
+                    </form>
+                    <br>
 
 
                     <table class = "table">
                         <tr>
                             <th>Hospital Number</th>
-                            <th>Name</th>
+                            <th>Full Name</th>
+                            <th>DOB</th>
+
                         </tr>
                       
                             @foreach ($patients as $patient)
                             <tr>
                             <td> {{ $patient -> hospital_number }}</td>
                             <td> {{ $patient -> name }}</td>
+                            <td> {{ $patient -> dob }}</td>
                             </tr>
                             @endforeach
+
                     </table>
                     
                     
