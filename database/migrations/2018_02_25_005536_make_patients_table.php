da10eee67885ff482b229441b0303c70154f88f0
Ruha Peera <ruha.rizwan@gmail.com>
2018-02-24 19:59:36 -0500

    creates migration and patients model

---
+++ database/migrations/2018_02_25_005536_make_patients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('Hospital Number');
+            $table->date('Date of Birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
