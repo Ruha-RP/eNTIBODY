fed2b95c8936761dd6011d561d2b5bf21710e32b
Ruha Peera <ruha.rizwan@gmail.com>
2018-03-07 11:46:32 -0500

    designs input patient page

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -4,29 +4,33 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">USER Dashboard</div>
+
+            <div class="card border-warning mb-3">
+                <div class="card-header">
+                    <span class="is-size-3" style="color:#26547C;">
+                        Blood Sciences Department
+                    </span>
+             </div>
 
                 <div class="card-body">
-                    <h1>Add New Patient</h1>
-                    <hr>
+                    <p class="is-size-5" style="color:#26547C;"><strong style="color:#EF476F;">Enter Patient Details</strong></p>
 
                     <form action="/patients" method="post">
 
                         {{ csrf_field() }}
 
                         <div class="form-group">
-                        <label for="title">Name</label>
+                        <label for="title" style="color:#26547C;">Name</label>
                         <input type="text" class="form-control" id="name"  name="name">
                         </div>
 
                         <div class="form-group">
-                        <label for="hospital_number">Hospital Number</label>
+                        <label for="hospital_number" style="color:#26547C;">Hospital Number</label>
                         <input type="number" class="form-control" id="hospital_number" name="hospital_number">
                         </div>
 
                         <div class="form-group">
-                        <label for="dob">DOB</label>
+                        <label for="dob" style="color:#26547C;">Date of Birth</label>
                         <input type="date" class="form-control" id="dob" name="dob">
                         </div>
 
@@ -39,7 +43,7 @@
                         </ul>
                         </div>
                         @endif
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="button is-primary is-outlined" style="color:#26547C;">Submit</button>
                         </form>             
             </div>
 
