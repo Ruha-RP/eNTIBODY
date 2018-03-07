2bb0654dc08ecd6af123878b206832148ae85416
Ruha Peera <ruha.rizwan@gmail.com>
2018-03-07 00:14:59 -0500

    nav bar styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Bulma CSS Framework -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css";>
     <!-- Font Awesome -->
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js";></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js";></script>
     <!-- In-built Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -26,11 +26,19 @@
 <body>
 
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+
+
+
+        <!-- THE NAVIGATION BAR -->
+        <nav class="navbar is-primary navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <span class="icon">
+                    <i class="fas fa-dna is-size-2" style="color:#26547C;"></i>
+                 </span>
+                <a class="navbar-item has-text-white is-size-3" href="{{ url('/') }}">
                     {{ config('app.name', 'LabTest') }}
                 </a>
+                
                 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -46,8 +54,19 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+
+                            <li>
+                                <a class="nav-link has-text-white is-size-4" href="{{ route('login') }}"><span class="icon">
+                                    <i class="fas fa-user-md" style="color:#26547C;"></i>
+                                </span> Login</a>
+                            </li>
+
+                            <li>
+                                <a class="nav-link has-text-white is-size-4" href="{{ route('register') }}"><span class="icon">
+                                    <i class="far fa-clipboard" style="color:#26547C;"></i>
+                                 </span> Register</a>
+                            </li>
+
                         @else
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -70,6 +89,8 @@
                 </div>
             </div>
         </nav>
+
+
 
         <main class="py-4">
             @yield('content')
