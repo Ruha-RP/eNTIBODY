12da41fa8422ef854ea9740b374c9df6b23e3f4a
Ruha Peera <ruha.rizwan@gmail.com>
2018-03-07 12:39:49 -0500

    interpret result designed

--- resources/views/inputresult.blade.php
+++ resources/views/inputresult.blade.php
@@ -4,13 +4,21 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">USER Dashboard</div>
 
-                
+            <div class="card border-warning mb-3">
+                <div class="card-header">
+                    <span class="is-size-3" style="color:#26547C;">
+                        Blood Sciences Department
+                    </span>
+            </div>
+            
+            <div class="card-body">
+                    <p class="is-size-5" style="color:#26547C;"><strong style="color:#EF476F;">Input your Observations based on the reactions on the microplate</strong></p>
+            </div>
 
+            <div class="card-body">
 
-                <!-- The Form -->
+              <!-- The Form -->
 
                 <!-- Opening the form -->
                 {!! Form::open(['action' => 'TestController@store', 'class' => 'ajax', 'method' => 'POST']) !!}
@@ -23,6 +31,7 @@
                     
                     <!-- The Positive Control Well -->
                     <div class="form-group required">
+                    <i class="fas fa-plus is-size-5 " style="color:#EF476F;"></i>
                     {!! Form::label('posC', 'Positive Control') !!}
                     {{ Form::select('posC', [
                     'Agglutination' => 'Agglutination', 
@@ -32,6 +41,7 @@
 
                     <!-- The Negative Control Well -->
                     <div class="form-group required">
+                    <i class="fas fa-minus is-size-5 " style="color:#EF476F;"></i>
                     {!! Form::label('negC', 'Negative Control') !!}
                     {{ Form::select('negC', [
                     'Agglutination' => 'Agglutination', 
@@ -40,6 +50,7 @@
 
                     <!-- The d1 Reagent Well -->
                     <div class="form-group required">
+                    <i class="fas fa-tint is-size-5 " style="color:#EF476F;"></i>
                     {!! Form::label('d1', 'Reagent D1') !!}
                     {{ Form::select('d1', [
                     'Agglutination' => 'Agglutination', 
@@ -48,6 +59,7 @@
 
                     <!-- The d2 Reagent Well -->
                     <div class="form-group required">
+                    <i class="fas fa-tint is-size-5 " style="color:#EF476F;"></i>
                     {!! Form::label('d2', 'Reagent D2') !!}
                     {{ Form::select('d2', [
                     'Agglutination' => 'Agglutination', 
@@ -56,31 +68,32 @@
 
                     <!-- The Submit button -->
                     <div class="form-group">
+                    <i class="fas fa-stethoscope is-size-5 " style="color:#26547C;"></i>
                     {!! Form::button('View result', ['class' => 'button',
                     'data-toggle' => 'modal' ,
                     'data-target'=> '#interpretResultModal',
-                    'id' => 'interpret'
+                    'id' => 'interpret',
+                    'class'=>'button is-primary is-outlined',
+                    'style'=>'color:#26547C'
                     ]) !!}
                     </div>
 
                     <!-- The Submit button -->
                     <div class="form-group">
-                    {!! Form::submit('Submit Diagnosis', ['class' => 'button'
+                    <i class="fas fa-clipboard-list is-size-5 " style="color:#26547C;"></i>
+                    {!! Form::submit('Submit Diagnosis', [
+                    'class'=>'button is-primary is-outlined',
+                    'style'=>'color:#26547C'
                     ]) !!}
                     </div>
 
               
 
+            </div>
 
-     <!-- To open Modal for Interpeting Results -->
-                    <button 
-                       id="result-section" 
-                       type="button" 
-                       class="button" 
-                       data-toggle="modal" 
-                       data-target="#interpretResultModal">
-                       Interpret Observations
-                    </button>
+                
+
+
 
                     
                 </div>
@@ -104,14 +117,16 @@
           aria-label="Close" >
           <span aria-hidden="true">&times;</span></button>
         <h4 class="modal-title" 
-        id="interpretResultModalLabel">Input Patient Details</h4>
+        id="interpretResultModalLabel"></h4>
       </div>
       <div class="modal-body">
-        <p>
-        The Result is ... 
+        <p class="is-size-5" style="color:#26547C;">
+        Result:
         </p>
         <div class="alert alert-success">
-          <div id="diagnosis">Diagnosis</div>
+          <div id="diagnosis" class="is-size-5" style="color:#26547C;">Please Enter your Observations</div><br>
+          <div>Enter Submit if results are correct.</div>
+
         </div>
 
       </div>
