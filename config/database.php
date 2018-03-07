81a9eabb2a34e2d2d59a526d5cc772fdfb2978d2
Ruha Peera <ruha.rizwan@gmail.com>
2018-03-07 10:14:50 -0500

    db credentials

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'b2mbugl97jqk6f3t'),
+            'username' => env('DB_USERNAME', 'yin88o603pjazhgx'),
+            'password' => env('DB_PASSWORD', 'pyileduumn3x38o0'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
