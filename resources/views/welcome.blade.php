becf5484ecc2492cf97624df324c13927c538e3a
Ruha Peera <ruha.rizwan@gmail.com>
2018-03-07 23:18:32 -0500

    trying to chenage deployed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-
+@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -36,7 +34,7 @@
     </div>
 </div>
 
-
+@endsection
 
 
                         
